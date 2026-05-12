2ff0f3caee05c8308268ff227f43f82941880857
murilobauck <murilobauck2@gmail.com>
2026-05-12 17:15:10 -0300

    remove(cleanup): deletar modulo de login e shared nao utilizados

--- src/pages/Login/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from '../../features/auth';
-
-export const Login = () => {
-  return (
-    <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '1rem' }}>
-      <LoginForm />
-    </div>
-  );
-};
